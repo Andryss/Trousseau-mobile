587e4a0aa77a0a5171a425a1483dce151b577e25
Andryss <andr.krivosheev@gmail.com>
2025-02-17 23:57:01 +0300

    Change theme

--- app/src/main/java/ru/andryss/trousseau/MainActivity.kt
+++ app/src/main/java/ru/andryss/trousseau/MainActivity.kt
@@ -9,7 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import ru.andryss.trousseau.ui.theme.TrousseauTheme
+import ru.andryss.trousseau.theme.TrousseauTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
